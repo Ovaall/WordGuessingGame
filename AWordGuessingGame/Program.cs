492bf7cd8cee49ed7d64742bb7d91387b4afa357
Valentyn Tukov <valentin.tukov@gmail.com>
2023-10-19 17:48:05 +0200

    First commit

---
+++ AWordGuessingGame/Program.cs
@@ -0,0 +1,72 @@
+﻿
+using System;
+
+namespace AWordGuessingGame;
+
+class HangmanGame
+{
+
+    static void Main()
+    {
+        
+        string secretWord = Utilities.GetRandomWord();
+
+        
+        int attemptsLeft = 10; 
+        string guessedWord = new string('_', secretWord.Length);
+        string incorrectGuesses = "";
+
+        
+        while (attemptsLeft > 0)
+        {
+            
+            Console.WriteLine($"Word: {guessedWord}");
+            Console.WriteLine($"Incorrect Guesses: {incorrectGuesses}");
+            Console.WriteLine($"Attempts Left: {attemptsLeft}");
+
+            
+            Console.Write("Enter a letter: ");
+            char guess = char.ToLower(Console.ReadKey().KeyChar);
+
+            
+            if (secretWord.Contains(guess))
+            {
+                
+                for (int i = 0; i < secretWord.Length; i++)
+                {
+                    if (secretWord[i] == guess)
+                    {
+                        guessedWord = guessedWord.Substring(0, i) + guess + guessedWord.Substring(i + 1);
+                    }
+                }
+
+                
+                if (guessedWord == secretWord)
+                {
+                    Console.WriteLine($"\nCongratulations! You guessed the word: {secretWord}");
+                    break;
+                }
+            }
+            else
+            {
+                
+                Console.WriteLine("\nIncorrect guess. Try again.");
+                attemptsLeft--;
+                incorrectGuesses += guess;
+            }
+        }
+
+        
+        if (attemptsLeft == 0)
+        {
+            Console.WriteLine($"\nSorry, you're out of attempts. The word was: {secretWord}");
+        }
+    }
+
+}
+
+
+    
+    
+
+
---
+++ AWordGuessingGame/Utilities.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System.Numerics;
+
+namespace AWordGuessingGame
+{
+    internal class Utilities
+    {
+        public static string GetRandomWord()
+        {
+            
+            string[] words = {"krakow", "located", "southern","Poland", "following", "communes", "border"};
+
+            
+            Random random = new Random();
+            int index = random.Next(words.Length);
+
+            
+            return words[index];
+        }
+    }
+}
